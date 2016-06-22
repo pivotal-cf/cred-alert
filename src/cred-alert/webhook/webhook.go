5fb975b8c0b0692b91014d3959f367a6f86318b9
Tony Wong <twong@pivotal.io>
2016-06-22 11:03:53 -0400

    [#121244805] - display received webhook info in logs
    Signed-off-by: Jesse Malone <jmalone@pivotal.io>

--- src/cred-alert/webhook/webhook.go
+++ src/cred-alert/webhook/webhook.go
@@ -2,6 +2,7 @@ package webhook
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/google/go-github/github"
@@ -22,5 +23,10 @@ func HandleWebhook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	fmt.Printf("Received a webhook. Before: %s, After: %s\n", *event.Before, *event.After)
+	fmt.Printf("Owner: %s, Repo Name: %s\n", *event.Repo.Owner.Name, *event.Repo.Name)
+	if event.Repo.FullName != nil {
+		fmt.Printf("Repo Fullname: %s\n", *event.Repo.FullName)
+	}
 	w.WriteHeader(http.StatusOK)
 }
