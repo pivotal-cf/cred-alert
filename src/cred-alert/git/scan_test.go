b0bd97873c10b09b82acd32bfa75dc4ddfc70a4e
Tony Wong <twong@pivotal.io>
2016-06-22 18:13:00 -0400

    [#121244857] - scan diffs from push events, log results
    Signed-off-by: Jesse Malone <jmalone@pivotal.io>

---
+++ src/cred-alert/git/scan_test.go
@@ -0,0 +1,32 @@
+package git_test
+
+import (
+	"cred-alert/git"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Scan", func() {
+
+	shortDiff := `diff --git a/spec/integration/git-secrets-pattern-tests.txt b/spec/integration/git-secrets-pattern-tests.txt
+index 940393e..fa5a232 100644
+--- a/spec/integration/git-secrets-pattern-tests.txt
++++ b/spec/integration/git-secrets-pattern-tests.txt
+@@ -28,3 +28,3 @@ private_key = "should_match" # TODO: comments shouldn't have an effect
+ private_key '$should_match'
+ # Should Not Match
++
+@@ -67,6 +75,5 @@ private_key: "should not match"
+ private_key: "too-short" # should_not_match
+ private_key: "fake_should_not_match"
++private_key: "should_match"
++private_key: "FaKe_should_not_match"
++private_key: "ExAmPlE_should_not_match"
+`
+	It("scans a diff and return Lines", func() {
+		matchingLines := git.Scan(shortDiff)
+		Expect(len(matchingLines)).To(Equal(2))
+	})
+
+})
--- src/cred-alert/webhook/webhook.go
+++ src/cred-alert/webhook/webhook.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+
+	"cred-alert/git"
+	myGithub "cred-alert/github"
 
 	"github.com/google/go-github/github"
 )
@@ -23,6 +27,10 @@ func HandleWebhook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	handlePushEvent(w, event)
+}
+
+func handlePushEvent(w http.ResponseWriter, event github.PushEvent) {
 	if event.Repo != nil {
 		fmt.Printf("Owner: %s, Repo Name: %s\n", *event.Repo.Owner.Name, *event.Repo.Name)
 	}
@@ -34,5 +42,36 @@ func HandleWebhook(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Printf("Received a webhook. Before: %s, After: %s\n", *event.Before, *event.After)
+
 	w.WriteHeader(http.StatusOK)
+
+	go scanPushEvent(event)
+}
+
+func scanPushEvent(event github.PushEvent) {
+	diff, err := fetchDiff(event)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "request error: ", err)
+	}
+
+	lines := git.Scan(diff)
+	for _, line := range lines {
+		fmt.Printf("Found match in repo: %s, file: %s, After SHA: %s, line number: %d\n",
+			*event.Repo.FullName,
+			line.Path,
+			*event.After,
+			line.LineNumber)
+	}
+}
+
+func fetchDiff(event github.PushEvent) (string, error) {
+	httpClient := &http.Client{}
+	githubClient := myGithub.NewClient("https://api.github.com/", httpClient)
+
+	diff, err := githubClient.CompareRefs(*event.Repo.Owner.Name, *event.Repo.Name, *event.Before, *event.After)
+	if err != nil {
+		return "", err
+	}
+
+	return diff, nil
 }
