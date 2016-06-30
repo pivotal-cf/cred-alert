a5191faa9e6ce318058f94c71b13e17c38d8be84
Tony Wong <twong@pivotal.io>
2016-06-30 10:33:32 -0400

    [#121957927]
      - implement the file scanner
    
    Signed-off-by: Jesse Malone <jmalone@pivotal.io>
    Signed-off-by: Tony Wong <twong@pivotal.io>

--- src/cred-alert/scanners/file/file_scanner.go
+++ src/cred-alert/scanners/file/file_scanner.go
@@ -3,24 +3,47 @@ package file
 import (
 	"bufio"
 	"cred-alert/sniff"
+	"fmt"
 	"os"
 
 	"github.com/pivotal-golang/lager"
 )
 
-type FileScanner struct {
-	file        *os.File
-	fileScanner *bufio.Scanner
+type fileScanner struct {
+	path         string
+	bufioScanner *bufio.Scanner
+	lineNumber   int
 }
 
-func NewFileScanner(file *os.File) *FileScanner {
-	return nil
+func NewFileScanner(file *os.File) *fileScanner {
+	bufioScanner := bufio.NewScanner(file)
+
+	return &fileScanner{
+		path:         file.Name(),
+		bufioScanner: bufioScanner,
+	}
 }
 
-func (s *FileScanner) Scan(logger lager.Logger) bool {
-	return false
+func (s *fileScanner) Scan(logger lager.Logger) bool {
+	logger = logger.Session("file-scanner")
+
+	success := s.bufioScanner.Scan()
+
+	if err := s.bufioScanner.Err(); err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	if success {
+		s.lineNumber++
+	}
+	return success
 }
 
-func (s *FileScanner) Line() *sniff.Line {
-	return new(sniff.Line)
+func (s *fileScanner) Line() *sniff.Line {
+	return &sniff.Line{
+		Content:    s.bufioScanner.Text(),
+		LineNumber: s.lineNumber,
+		Path:       s.path,
+	}
 }
--- src/cred-alert/scanners/file/file_scanner_test.go
+++ src/cred-alert/scanners/file/file_scanner_test.go
@@ -1,28 +1,73 @@
 package file_test
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"cred-alert/scanners/file"
+	"cred-alert/sniff"
+
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pivotal-golang/lager"
+	"github.com/pivotal-golang/lager/lagertest"
 )
 
 var _ = Describe("File", func() {
+	var (
+		myFile       *os.File
+		tempFilePath string
+		fileContent  string
+		fileScanner  sniff.Scanner
+		logger       lager.Logger
+	)
+
 	BeforeEach(func() {
+		fileContent = `line1
+line2
+line3`
+
+		tempFilePath = fmt.Sprintf("%sfile-scanner-test-temp", os.TempDir())
+		if err := ioutil.WriteFile(tempFilePath, []byte(fileContent), 0644); err != nil {
+			fmt.Println(err)
+		}
+		myFile, _ = os.Open(tempFilePath)
+
+		logger = lagertest.NewTestLogger("file-scanner")
 	})
 
 	AfterEach(func() {
+		myFile.Close()
+		os.Remove(myFile.Name())
 	})
 
 	JustBeforeEach(func() {
+		fileScanner = file.NewFileScanner(myFile)
 	})
 
 	It("scans lines from a file", func() {
+		Expect(fileScanner.Scan(logger)).To(BeTrue())
+		Expect(fileScanner.Scan(logger)).To(BeTrue())
+		Expect(fileScanner.Scan(logger)).To(BeTrue())
+		Expect(fileScanner.Scan(logger)).To(BeFalse())
 	})
 
 	It("returns the current line", func() {
+		Expect(fileScanner.Scan(logger)).To(BeTrue())
+		line := fileScanner.Line()
 
+		Expect(line.Path).To(Equal(tempFilePath))
+		Expect(line.Content).To(Equal("line1"))
+		Expect(line.LineNumber).To(Equal(1))
 	})
 
 	It("keeps track of line numbers", func() {
-
+		Expect(fileScanner.Scan(logger)).To(BeTrue())
+		Expect(fileScanner.Scan(logger)).To(BeTrue())
+		Expect(fileScanner.Scan(logger)).To(BeTrue())
+		line := fileScanner.Line()
+		Expect(line.LineNumber).To(Equal(3))
 	})
 
 })
