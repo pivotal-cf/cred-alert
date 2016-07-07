5ae37ed4611c22ef4c01da3793d2ab9ac4518c49
aram price <aprice@pivotal.io>
2016-07-07 15:52:34 -0400

    [#123968501] detect crypt encrypted passwords
    Signed-off-by: Tony Wong <twong@pivotal.io>

--- src/cred-alert/sniff/patterns/default_matcher.go
+++ src/cred-alert/sniff/patterns/default_matcher.go
@@ -1,17 +1,32 @@
 package patterns
 
+const generalPattern = `(?i)("|')*[A-Za-z0-9_-]*(secret|private[-_]?key|password|salt)["']*\s*(=|:|\s|:=|=>)\s*["'][A-Za-z0-9.$+=&\/_\\-]{12,}("|')`
+const awsAccessKeyIDPattern = `AKIA[A-Z0-9]{16}`
+const awsSecretAccessKeyPattern = `(?i)("|')?(aws)?_?(secret)?_?(access)?_?(key)("|')?\s*(:|=>|=)\s*("|')?[A-Za-z0-9/\+=]{40}("|')?`
+const awsAccountIDPattern = `(?i)("|')?(aws)?_?(account)_?(id)?("|')?\s*(:|=>|=)\s*("|')?[0-9]{4}\-?[0-9]{4}\-?[0-9]{4}("|')?`
+const cryptMD5Pattern = `\$1\$[a-zA-Z0-9./]{16}\$[a-zA-Z0-9./]{22}`
+const cryptSHA256Pattern = `\$5\$[a-zA-Z0-9./]{16}\$[a-zA-Z0-9./]{43}`
+const cryptSHA512Pattern = `\$6\$[a-zA-Z0-9./]{16}\$[a-zA-Z0-9./]{86}`
+
+const bashStringInterpolationPattern = `["]\$`
+const fakePattern = `(?i)fake`
+const examplePattern = `(?i)example`
+
 func DefaultMatcher() *Matcher {
 	return NewMatcher(
 		[]string{
-			`AKIA[A-Z0-9]{16}`,
-			`(?i)("|')?(aws)?_?(secret)?_?(access)?_?(key)("|')?\s*(:|=>|=)\s*("|')?[A-Za-z0-9/\+=]{40}("|')?`,
-			`(?i)("|')?(aws)?_?(account)_?(id)?("|')?\s*(:|=>|=)\s*("|')?[0-9]{4}\-?[0-9]{4}\-?[0-9]{4}("|')?`,
-			`(?i)("|')*[A-Za-z0-9_-]*(secret|private[-_]?key|password|salt)["']*\s*(=|:|\s|:=|=>)\s*["'][A-Za-z0-9.$+=&\/_\\-]{12,}("|')`,
+			generalPattern,
+			awsAccessKeyIDPattern,
+			awsSecretAccessKeyPattern,
+			awsAccountIDPattern,
+			cryptMD5Pattern,
+			cryptSHA256Pattern,
+			cryptSHA512Pattern,
 		},
 		[]string{
-			`["]\$`,
-			`(?i)fake`,
-			`(?i)example`,
+			bashStringInterpolationPattern,
+			fakePattern,
+			examplePattern,
 		},
 	)
 }
