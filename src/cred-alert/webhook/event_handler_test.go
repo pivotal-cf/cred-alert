66fce7cf564aa5832c83574f82c8c91bedca1530
Christopher Brown <cbrown@pivotal.io>
2016-07-08 14:26:58 -0700

    move PushScan into its own file

--- src/cred-alert/webhook/event_handler_test.go
+++ src/cred-alert/webhook/event_handler_test.go
@@ -12,88 +12,8 @@ import (
 	"cred-alert/queue/queuefakes"
 	"cred-alert/webhook"
 
-	"github.com/google/go-github/github"
 	"github.com/pivotal-golang/lager/lagertest"
 )
-
-var _ = Describe("Extract", func() {
-	var (
-		event github.PushEvent
-	)
-
-	BeforeEach(func() {
-		event = github.PushEvent{
-			Before: github.String("commit-sha-0"),
-			Repo: &github.PushEventRepository{
-				Name: github.String("repository-name"),
-				Owner: &github.PushEventRepoOwner{
-					Name: github.String("repository-owner"),
-				},
-			},
-			Commits: []github.PushEventCommit{
-				{ID: github.String("commit-sha-1")},
-				{ID: github.String("commit-sha-2")},
-				{ID: github.String("commit-sha-3")},
-				{ID: github.String("commit-sha-4")},
-				{ID: github.String("commit-sha-5")},
-			},
-		}
-	})
-
-	It("can give us the first and last commit of the push", func() {
-		scan, valid := webhook.Extract(event)
-		Expect(valid).To(BeTrue())
-
-		Expect(scan.FirstCommit()).To(Equal("commit-sha-0"))
-		Expect(scan.LastCommit()).To(Equal("commit-sha-5"))
-	})
-
-	It("can extract a value object from a github push event", func() {
-		scan, valid := webhook.Extract(event)
-		Expect(valid).To(BeTrue())
-
-		Expect(scan.Owner).To(Equal("repository-owner"))
-		Expect(scan.Repository).To(Equal("repository-name"))
-		Expect(scan.Diffs).To(Equal([]webhook.PushScanDiff{
-			{Start: "commit-sha-0", End: "commit-sha-1"},
-			{Start: "commit-sha-1", End: "commit-sha-2"},
-			{Start: "commit-sha-2", End: "commit-sha-3"},
-			{Start: "commit-sha-3", End: "commit-sha-4"},
-			{Start: "commit-sha-4", End: "commit-sha-5"},
-		}))
-	})
-
-	It("can have a full repository name", func() {
-		scan, valid := webhook.Extract(event)
-		Expect(valid).To(BeTrue())
-
-		Expect(scan.Owner).To(Equal("repository-owner"))
-		Expect(scan.Repository).To(Equal("repository-name"))
-
-		Expect(scan.FullRepoName()).To(Equal("repository-owner/repository-name"))
-	})
-
-	It("can handle if there are no commits in a push (may not even be possible)", func() {
-		event.Commits = []github.PushEventCommit{}
-
-		_, valid := webhook.Extract(event)
-		Expect(valid).To(BeFalse())
-	})
-
-	It("is not valid if there is no before specified", func() {
-		event.Before = nil
-
-		_, valid := webhook.Extract(event)
-		Expect(valid).To(BeFalse())
-	})
-
-	It("is not valid if this is the initial push to the repository because the GitHub API doesn't allow this comparison", func() {
-		event.Before = github.String("0000000000000000000000000000000000000000")
-
-		_, valid := webhook.Extract(event)
-		Expect(valid).To(BeFalse())
-	})
-})
 
 var _ = Describe("EventHandler", func() {
 	var (
---
+++ src/cred-alert/webhook/push_scan.go
@@ -0,0 +1,67 @@
+package webhook
+
+import (
+	"fmt"
+
+	"github.com/google/go-github/github"
+)
+
+type PushScan struct {
+	Owner      string
+	Repository string
+
+	Diffs []PushScanDiff
+}
+
+func (p PushScan) FullRepoName() string {
+	return fmt.Sprintf("%s/%s", p.Owner, p.Repository)
+}
+
+func (p PushScan) FirstCommit() string {
+	return p.Diffs[0].Start
+}
+
+func (p PushScan) LastCommit() string {
+	return p.Diffs[len(p.Diffs)-1].End
+}
+
+type PushScanDiff struct {
+	Start string
+	End   string
+}
+
+const initalCommitParentHash = "0000000000000000000000000000000000000000"
+
+func Extract(event github.PushEvent) (PushScan, bool) {
+	if event.Before == nil || *event.Before == initalCommitParentHash {
+		return PushScan{}, false
+	}
+
+	if len(event.Commits) == 0 {
+		return PushScan{}, false
+	}
+
+	diffs := []PushScanDiff{
+		{Start: *event.Before, End: *event.Commits[0].ID},
+	}
+
+	for i, _ := range event.Commits {
+		if i == len(event.Commits)-1 {
+			break
+		}
+
+		start := *event.Commits[i].ID
+		end := *event.Commits[i+1].ID
+
+		diffs = append(diffs, PushScanDiff{
+			Start: start,
+			End:   end,
+		})
+	}
+
+	return PushScan{
+		Owner:      *event.Repo.Owner.Name,
+		Repository: *event.Repo.Name,
+		Diffs:      diffs,
+	}, true
+}
