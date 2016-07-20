638a0bd303f7ba5bc9e033d7cc171aa735a580bb
Christopher Brown <cbrown@pivotal.io>
2016-07-20 12:10:36 -0700

    simplify some portions of general regex
    [#125681231]
    
    Signed-off-by: Kris Hicks <khicks@pivotal.io>

--- src/cred-alert/sniff/patterns/default_matcher.go
+++ src/cred-alert/sniff/patterns/default_matcher.go
@@ -1,6 +1,7 @@
 package patterns
 
-const generalPattern = `(?i)("|')*[A-Za-z0-9_-]*(secret|private[-_]?key|password|salt)["']*\s*(=|:|\s|:=|=>)\s*["'][A-Za-z0-9.$+=&\/_\\-]{12,}("|')`
+const generalPattern = `(?i)["']?[A-Za-z0-9_-]*(secret|private[-_]?key|password|salt)["']?\s*(=|:|:=|=>)?\s*["'][A-Za-z0-9.$+=&\/_\\-]{12,}["']`
+
 const awsAccessKeyIDPattern = `AKIA[A-Z0-9]{16}`
 const awsSecretAccessKeyPattern = `(?i)("|')?(aws)?_?(secret)?_?(access)?_?(key)("|')?\s*(:|=>|=)\s*("|')?[A-Za-z0-9/\+=]{40}("|')?`
 const awsAccountIDPattern = `(?i)("|')?(aws)?_?(account)_?(id)?("|')?\s*(:|=>|=)\s*("|')?[0-9]{4}\-?[0-9]{4}\-?[0-9]{4}("|')?`
