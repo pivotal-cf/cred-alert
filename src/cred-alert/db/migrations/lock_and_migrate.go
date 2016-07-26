f44c591f3a6d8f432f8463728e13dbc004967d92
aram price <aprice@pivotal.io>
2016-07-26 15:17:27 -0700

    Improve logging in LockDBAndMigrate [#127118535]

--- src/cred-alert/db/migrations/lock_and_migrate.go
+++ src/cred-alert/db/migrations/lock_and_migrate.go
@@ -12,49 +12,69 @@ import (
 )
 
 func LockDBAndMigrate(logger lager.Logger, driver, dbURI string) (*gorm.DB, error) {
+	logger = logger.Session("lock-db-and-migrate")
+	logger.Info("starting")
+
+	lockDB, err := dbOpen(logger, driver, dbURI)
+	if err != nil {
+		logger.Fatal("failed", err)
+		return nil, err
+	}
+	defer lockDB.Close()
+
+	lockName := crc32.ChecksumIEEE([]byte(driver + dbURI))
+
+	for {
+		logger.Info("acquiring-lock")
+		_, err = lockDB.Exec(`SELECT GET_LOCK(?,10);`, lockName)
+		if err != nil {
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		logger.Info("migrating")
+		_, err = migration.OpenWith(driver, dbURI, Migrations, migration.DefaultGetVersion, setVersion)
+		if err != nil {
+			logger.Fatal("failed", err)
+		}
+
+		logger.Info("releasing-lock")
+		_, err = lockDB.Exec(`SELECT RELEASE_LOCK(?)`, lockName)
+		if err != nil {
+			logger.Error("failed", err)
+		}
+
+		break
+	}
+
+	logger.Info("done")
+	return gorm.Open(driver, dbURI)
+}
+
+func dbOpen(logger lager.Logger, driver, dbURI string) (*sql.DB, error) {
 	var err error
 	var lockDB *sql.DB
+
+	logger = logger.Session("db-open")
+	logger.Info("starting")
 
 	for {
 		lockDB, err = sql.Open(driver, dbURI)
 		if err != nil {
 			if strings.Contains(err.Error(), " dial ") {
-				logger.Error("failed-to-open-db-retrying", err)
+				logger.Error("retrying", err)
 				time.Sleep(5 * time.Second)
 				continue
 			}
+			logger.Fatal("failed", err)
 			return nil, err
 		}
 
 		break
 	}
 
-	lockName := crc32.ChecksumIEEE([]byte(driver + dbURI))
-
-	for {
-		_, err = lockDB.Exec(`SELECT GET_LOCK(?,10);`, lockName)
-		if err != nil {
-			logger.Error("failed-to-acquire-lock-retrying", err)
-			time.Sleep(5 * time.Second)
-			continue
-		}
-
-		logger.Info("migration-lock-acquired")
-		_, err = migration.OpenWith(driver, dbURI, Migrations, migration.DefaultGetVersion, setVersion)
-		if err != nil {
-			logger.Fatal("failed-to-run-migrations", err)
-		}
-
-		_, err = lockDB.Exec(`SELECT RELEASE_LOCK(?)`, lockName)
-		if err != nil {
-			logger.Error("failed-to-release-lock", err)
-		}
-
-		lockDB.Close()
-		break
-	}
-
-	return gorm.Open(driver, dbURI)
+	logger.Info("done")
+	return lockDB, err
 }
 
 func setVersion(tx migration.LimitedTx, version int) error {
