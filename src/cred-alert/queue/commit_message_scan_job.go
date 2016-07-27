35d3d4b8e87b287c11e95582d8bb315bbb2ed3bc
Tony Wong <twong@pivotal.io>
2016-07-27 17:30:18 -0400

    implement CommitMessageJob logging, notifying, and emitting to datadog [#122208753]

--- src/cred-alert/queue/commit_message_scan_job.go
+++ src/cred-alert/queue/commit_message_scan_job.go
@@ -3,6 +3,8 @@ package queue
 import (
 	"cred-alert/metrics"
 	"cred-alert/notifications"
+	"cred-alert/scanners"
+	"cred-alert/scanners/textscanner"
 	"cred-alert/sniff"
 
 	"github.com/pivotal-golang/lager"
@@ -36,5 +38,44 @@ func NewCommitMessageJob(
 }
 
 func (j *CommitMessageJob) Run(logger lager.Logger) error {
+	logger = logger.Session("scan-commit-message", lager.Data{
+		"owner":      j.Owner,
+		"repository": j.Repository,
+		"private":    j.Private,
+		"sha":        j.SHA,
+		"task-id":    j.id,
+	})
+
+	logger.Info("start")
+
+	textScanner := textscanner.New(j.Message)
+
+	err := j.sniffer.Sniff(logger, textScanner, j.createHandleViolation(logger))
+	if err != nil {
+		logger.Error("failed", err)
+		return err
+	}
+
+	logger.Info("done")
+
 	return nil
+}
+
+func (j *CommitMessageJob) createHandleViolation(logger lager.Logger) func(scanners.Line) error {
+	return func(line scanners.Line) error {
+		logger.Info("found-credentials")
+
+		privacyTag := "public"
+		if j.Private {
+			privacyTag = "private"
+		}
+
+		j.credentialCounter.Inc(logger, privacyTag, "commit-message")
+
+		if err := j.notifier.SendNotification(logger, j.Owner, j.SHA, line, j.Private); err != nil {
+			return err
+		}
+
+		return nil
+	}
 }
--- src/cred-alert/queue/commit_message_scan_job_test.go
+++ src/cred-alert/queue/commit_message_scan_job_test.go
@@ -5,11 +5,17 @@ import (
 	"cred-alert/metrics/metricsfakes"
 	"cred-alert/notifications/notificationsfakes"
 	"cred-alert/queue"
+	"cred-alert/scanners"
+	"cred-alert/sniff"
 	"cred-alert/sniff/snifffakes"
+	"errors"
+	"fmt"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 
+	"github.com/pivotal-golang/lager"
 	"github.com/pivotal-golang/lager/lagertest"
 )
 
@@ -24,6 +30,7 @@ var _ = Describe("Commit Message Scan Job", func() {
 			notifier          *notificationsfakes.FakeNotifier
 			sniffer           *snifffakes.FakeSniffer
 			credentialCounter *metricsfakes.FakeCounter
+			taskId            string
 		)
 
 		BeforeEach(func() {
@@ -33,6 +40,7 @@ var _ = Describe("Commit Message Scan Job", func() {
 				Repository: "repo",
 				SHA:        "sha",
 				Message:    "message",
+				Private:    true,
 			}
 			sniffer = new(snifffakes.FakeSniffer)
 			emitter = &metricsfakes.FakeEmitter{}
@@ -46,33 +54,95 @@ var _ = Describe("Commit Message Scan Job", func() {
 					panic("unexpected counter name! " + name)
 				}
 			}
+			taskId = "task-id"
 		})
 
 		JustBeforeEach(func() {
-			job = queue.NewCommitMessageJob(sniffer, emitter, notifier, plan, "id")
+			job = queue.NewCommitMessageJob(sniffer, emitter, notifier, plan, taskId)
+		})
+
+		It("logs basic info", func() {
+			err := job.Run(logger)
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(logger).To(gbytes.Say("scan-commit-message"))
+			Expect(logger).To(gbytes.Say(plan.Owner))
+			Expect(logger).To(gbytes.Say(fmt.Sprintf(`"private":%v`, plan.Private)))
+			Expect(logger).To(gbytes.Say(plan.Repository))
+			Expect(logger).To(gbytes.Say(plan.SHA))
+			Expect(logger).To(gbytes.Say(taskId))
 		})
 
 		Context("When the message contains a credential", func() {
+			violatingLine := scanners.Line{
+				LineNumber: 999,
+			}
+
 			BeforeEach(func() {
-				plan.Message = `password = "should_match"`
+				sniffer.SniffStub = func(logger lager.Logger, scanner sniff.Scanner, handleViolation func(scanners.Line) error) error {
+					return handleViolation(violatingLine)
+				}
 			})
 
-			It("logs the violations", func() {
+			It("logs the violation", func() {
+				err := job.Run(logger)
+
+				Expect(err).ToNot(HaveOccurred())
+				Expect(logger).To(gbytes.Say("found-credentials"))
 			})
 
 			It("emits the violations", func() {
-				Expect(true).To(BeTrue())
-				// err := job.Run(logger)
-				// Expect(err).NotTo(HaveOccurred())
+				job.Run(logger)
 
-				// Expect(credentialCounter.IncCallCount()).To(Equal(1))
-				// _, tags := credentialCounter.IncArgsForCall(0)
-				// Expect(tags).To(HaveLen(1))
-				// Expect(tags).To(ConsistOf("private"))
+				Expect(credentialCounter.IncCallCount()).To(Equal(1))
+				_, tags := credentialCounter.IncArgsForCall(0)
+				Expect(tags).To(ContainElement("private"))
+				Expect(tags).To(ContainElement("commit-message"))
 			})
 
 			It("notifies slack about the violations", func() {
+				job.Run(logger)
+
+				Expect(notifier.SendNotificationCallCount()).To(Equal(1))
+
+				_, repository, sha, line, private := notifier.SendNotificationArgsForCall(0)
+
+				Expect(repository).To(Equal(plan.Owner))
+				Expect(sha).To(Equal(plan.SHA))
+				Expect(line).To(Equal(violatingLine))
+				Expect(private).To(Equal(plan.Private))
 			})
+
+			Context("When the repo is public", func() {
+				BeforeEach(func() {
+					plan.Private = false
+				})
+
+				It("log, emits, notifies the public-ness", func() {
+					job.Run(logger)
+
+					Expect(logger).To(gbytes.Say(fmt.Sprintf(`"private":%v`, false)))
+
+					Expect(credentialCounter.IncCallCount()).To(Equal(1))
+					_, tags := credentialCounter.IncArgsForCall(0)
+					Expect(tags).To(ContainElement("public"))
+
+					_, _, _, _, notificationPrivacy := notifier.SendNotificationArgsForCall(0)
+					Expect(notificationPrivacy).To(Equal(false))
+				})
+			})
+
+			Context("when the notification fails to send", func() {
+				BeforeEach(func() {
+					notifier.SendNotificationReturns(errors.New("disaster"))
+				})
+
+				It("fails the job", func() {
+					err := job.Run(logger)
+					Expect(err).To(HaveOccurred())
+				})
+			})
+
 		})
 
 	})
