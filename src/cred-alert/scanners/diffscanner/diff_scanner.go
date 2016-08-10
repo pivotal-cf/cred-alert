80db82b06c67bff42b96a5c09601bfea6e651a41
Kris Hicks <khicks@pivotal.io>
2016-08-09 17:43:36 -0700

    Simplify diffscanner
    - Remove private methods that became unnecessary
    - Remove Hunk struct; it's not required
    - Simplify regexes, don't capture things that don't need capturing
    
    This refactoring makes it more clear how the scanner works, in that it
    scans until it finds a file header, persisting that path, then scans
    until it finds a hunk header, persisting its associated line number, and
    then scans until it finds a context line within the hunk, at which point
    it returns true. If it finds no context line it returns false.
    
    What I find curious about this code is that it only increments the line
    within the hunk when it finds a matching context line; those that do not
    match are not counted, but perhaps should.

--- src/cred-alert/scanners/diffscanner/diff_scanner.go
+++ src/cred-alert/scanners/diffscanner/diff_scanner.go
@@ -2,17 +2,21 @@ package diffscanner
 
 import (
 	"cred-alert/scanners"
-	"errors"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"code.cloudfoundry.org/lager"
 )
 
+var fileHeaderPattern = regexp.MustCompile(`^\+\+\+\s\w\/(.*)$`)
+var contextAddedLinePattern = regexp.MustCompile(`^(?:\s|\+)`)
+var plusMinusSpacePattern = regexp.MustCompile(`^(?:\s|\+|\-)(.*)`)
+var hunkHeaderRegexp = regexp.MustCompile(`^@@.*\+(\d+),?\d+?\s@@`)
+
 type DiffScanner struct {
 	diff              []string
 	cursor            int
-	currentHunk       *Hunk
 	currentPath       string
 	currentLineNumber int
 }
@@ -27,145 +31,60 @@ func NewDiffScanner(diff string) *DiffScanner {
 func (d *DiffScanner) Scan(logger lager.Logger) bool {
 	logger = logger.Session("diff-scanner").Session("scan")
 	logger.Debug("starting")
+	defer logger.Debug("done")
 
-	// read information about hunk
-	var isContentLine bool
-	for !isContentLine {
-		l := logger.WithData(lager.Data{
-			"current-line-number": d.currentLineNumber,
-		})
-
+	for {
 		d.cursor++
-
 		if d.cursor >= len(d.diff) {
-			logger.Debug("passed-last-line")
-			logger.Debug("done")
-			return false
+			break
 		}
 
-		l.Debug("considering-line")
 		rawLine := d.diff[d.cursor]
 
-		d.scanHeader(l, rawLine)
-		isContentLine = d.scanHunk(l, rawLine)
+		matches := fileHeaderPattern.FindStringSubmatch(rawLine)
+		if len(matches) == 2 {
+			d.currentPath = matches[1]
+			continue
+		}
+
+		matches = hunkHeaderRegexp.FindStringSubmatch(rawLine)
+		if len(matches) == 2 {
+			startLine, err := strconv.Atoi(matches[1])
+			if err != nil {
+				logger.Error("failed", err)
+				break
+			}
+			d.currentLineNumber = startLine - 1
+			continue
+		}
+
+		matches = contextAddedLinePattern.FindStringSubmatch(rawLine)
+		if len(matches) == 1 {
+			d.currentLineNumber++
+			return true
+		}
 	}
 
-	logger.Debug("done")
-	return true
-}
-
-func (d *DiffScanner) Line(logger lager.Logger) *scanners.Line {
-	lineNumber := d.currentLineNumber
-	path := d.currentHunk.path
-
-	logger = logger.Session("line", lager.Data{
-		"line-number": lineNumber,
-		"path":        path,
-	})
-	logger.Debug("starting")
-
-	content, err := diffContents(d.diff[d.cursor])
-	if err != nil {
-		logger.Error("setting content to ''", err)
-	}
-
-	logger.Debug("done")
-	return &scanners.Line{
-		Content:    []byte(content),
-		LineNumber: lineNumber,
-		Path:       path,
-	}
-}
-
-func (d *DiffScanner) scanHeader(logger lager.Logger, rawLine string) {
-	logger = logger.Session("scan-header")
-	logger.Debug("starting")
-
-	nextLineNumber := d.currentLineNumber + 1
-
-	if !isInHeader(nextLineNumber, d.currentHunk) {
-		logger.Debug("not-in-header")
-		logger.Debug("done")
-		return
-	}
-
-	path, err := fileHeader(rawLine, nextLineNumber, d.currentHunk)
-	if err == nil {
-		logger.Debug("detected-file-header")
-		d.currentPath = path
-		d.currentHunk = nil
-	}
-
-	startLine, length, err := hunkHeader(logger, rawLine)
-	if err == nil {
-		logger.Debug("detected-hunk-header")
-		d.currentHunk = newHunk(d.currentPath, startLine, length)
-
-		// the hunk header exists immeidately before the first line
-		d.currentLineNumber = startLine - 1
-	}
-
-	logger.Debug("done")
-}
-
-func (d *DiffScanner) scanHunk(logger lager.Logger, rawLine string) bool {
-	logger = logger.Session("scan-hunk")
-	logger.Debug("starting")
-	nextLineNumber := d.currentLineNumber + 1
-
-	if isInHeader(nextLineNumber, d.currentHunk) {
-		logger.Debug("done")
-		return false
-	}
-
-	if contextOrAddedLine(rawLine) {
-		logger.Debug("detected-content-line")
-		d.currentLineNumber = nextLineNumber
-		logger.Debug("done")
-		return true
-	}
-
-	logger.Debug("done")
 	return false
 }
 
-var fileHeaderPattern = regexp.MustCompile(`^\+\+\+\s\w\/(.*)$`)
-var contextAddedLinePattern = regexp.MustCompile(`^(\s|\+)`)
-var plusMinusSpacePattern = regexp.MustCompile(`^(\s|\+|\-)(.*)`)
+func (d *DiffScanner) Line(logger lager.Logger) *scanners.Line {
+	logger = logger.Session("line", lager.Data{
+		"line-number": d.currentLineNumber,
+		"path":        d.currentPath,
+	})
+	logger.Debug("starting")
+	defer logger.Debug("done")
 
-func fileHeader(rawLine string, currentLineNumber int, currentHunk *Hunk) (string, error) {
-	if !isInHeader(currentLineNumber, currentHunk) {
-		return "", errors.New("Still processing a hunk, not a file header")
-	}
-	return readFileHeader(rawLine)
-}
-
-func isInHeader(currentLineNumber int, currentHunk *Hunk) bool {
-	if currentHunk == nil {
-		return true
+	var content string
+	matches := plusMinusSpacePattern.FindStringSubmatch(d.diff[d.cursor])
+	if len(matches) == 2 {
+		content = matches[1]
 	}
 
-	return currentHunk.endOfHunk(currentLineNumber)
-}
-
-func readFileHeader(line string) (string, error) {
-	matches := fileHeaderPattern.FindStringSubmatch(line)
-	if len(matches) < 2 {
-		return "", errors.New("Not a path")
+	return &scanners.Line{
+		Content:    []byte(content),
+		LineNumber: d.currentLineNumber,
+		Path:       d.currentPath,
 	}
-	return matches[1], nil
-}
-
-func contextOrAddedLine(rawLine string) bool {
-	matches := contextAddedLinePattern.FindStringSubmatch(rawLine)
-	return len(matches) >= 2
-}
-
-func diffContents(rawLine string) (string, error) {
-	matches := plusMinusSpacePattern.FindStringSubmatch(rawLine) // detect +, -, or <space>
-	if len(matches) >= 3 {
-		return matches[2], nil
-	}
-
-	return "", errors.New("Could not match content string")
 }
