da43b9f42e29e1730f51370e361445fb2b58eeed
Kris Hicks <khicks@pivotal.io>
2016-08-19 16:57:27 -0700

    Add update command

--- src/cred-alert/commands/cred_alert.go
+++ src/cred-alert/commands/cred_alert.go
@@ -1,7 +1,8 @@
 package commands
 
 type CredAlertCommand struct {
-	Scan ScanCommand `command:"scan" description:"Scan an archive, Git diff, or input from STDIN"`
+	Scan   ScanCommand   `command:"scan" description:"Scan an archive, Git diff, or input from STDIN"`
+	Update UpdateCommand `command:"update" description:"Update cred-alert to the latest version"`
 }
 
 var CredAlert CredAlertCommand
