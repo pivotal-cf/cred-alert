f689298812edc45fa7d44bbfd568cf2e0875ebc1
Kris Hicks <khicks@pivotal.io>
2016-10-24 17:29:04 -0700

    Refactor rescanner
    * It can now be canceled while doing work
    * Add logging

--- src/cred-alert/revok/rescanner.go
+++ src/cred-alert/revok/rescanner.go
@@ -45,71 +45,84 @@ func (r *Rescanner) Run(signals <-chan os.Signal, ready chan<- struct{}) error {
 	logger := r.logger.Session("rescanner")
 	logger.Info("started")
 
-	close(ready)
-
 	defer logger.Info("done")
 
-	_ = r.work(logger)
+	close(ready)
 
-	<-signals
-
-	return nil
-}
-
-func (r *Rescanner) work(logger lager.Logger) error {
 	priorScans, err := r.scanRepo.ScansNotYetRunWithVersion(logger, sniff.RulesVersion)
 	if err != nil {
 		logger.Error("failed-getting-prior-scans", err)
+	}
+
+	if len(priorScans) > 0 {
+		for _, priorScan := range priorScans {
+			select {
+			case <-signals:
+				return nil
+			default:
+				err := r.work(logger, priorScan)
+				if err != nil {
+					r.failedCounter.Inc(logger)
+					logger.Error("failed-to-rescan", err, lager.Data{
+						"scan-id": priorScan.ID,
+					})
+				}
+			}
+		}
+	}
+
+	logger.Info("all-scans-up-to-date")
+	<-signals
+	return nil
+}
+
+func (r *Rescanner) work(logger lager.Logger, priorScan db.PriorScan) error {
+	logger.Info("rescanning", lager.Data{
+		"scan-id": priorScan.ID,
+	})
+
+	oldCredentials, err := r.credRepo.ForScanWithID(priorScan.ID)
+	if err != nil {
+		logger.Error("failed-getting-prior-credentials", err)
 		return err
 	}
 
-	if len(priorScans) == 0 {
-		logger.Info("no-prior-scans-for-rules-version", lager.Data{
-			"rules_version": sniff.RulesVersion - 1,
-		})
-		return nil
+	credMap := map[string]db.Credential{}
+	for _, cred := range oldCredentials {
+		credMap[cred.Hash()] = cred
 	}
 
-	for _, priorScan := range priorScans {
-		oldCredentials, err := r.credRepo.ForScanWithID(priorScan.ID)
+	newCredentials, err := r.scanner.ScanNoNotify(
+		logger,
+		priorScan.Owner,
+		priorScan.Repository,
+		priorScan.StartSHA,
+		priorScan.StopSHA,
+	)
+	if err != nil {
+		return err
+	}
+
+	r.successCounter.Inc(logger)
+
+	var batch []notifications.Notification
+	for _, cred := range newCredentials {
+		if _, ok := credMap[cred.Hash()]; !ok {
+			batch = append(batch, notifications.Notification{
+				Owner:      cred.Owner,
+				Repository: cred.Repository,
+				SHA:        cred.SHA,
+				Path:       cred.Path,
+				LineNumber: cred.LineNumber,
+				Private:    cred.Private,
+			})
+		}
+	}
+
+	if len(batch) > 0 {
+		err = r.notifier.SendBatchNotification(logger, batch)
 		if err != nil {
-			r.failedCounter.Inc(logger)
-			logger.Error("failed-getting-prior-credentials", err)
-			continue
-		}
-
-		credMap := map[string]db.Credential{}
-		for _, cred := range oldCredentials {
-			credMap[cred.Hash()] = cred
-		}
-
-		newCredentials, err := r.scanner.ScanNoNotify(logger, priorScan.Owner, priorScan.Repository, priorScan.StartSHA, priorScan.StopSHA)
-		if err != nil {
-			logger.Error("failed-to-scan", err)
-			r.failedCounter.Inc(logger)
-		} else {
-			r.successCounter.Inc(logger)
-		}
-
-		var batch []notifications.Notification
-		for _, cred := range newCredentials {
-			if _, ok := credMap[cred.Hash()]; !ok {
-				batch = append(batch, notifications.Notification{
-					Owner:      cred.Owner,
-					Repository: cred.Repository,
-					SHA:        cred.SHA,
-					Path:       cred.Path,
-					LineNumber: cred.LineNumber,
-					Private:    cred.Private,
-				})
-			}
-		}
-
-		if len(batch) > 0 {
-			err = r.notifier.SendBatchNotification(logger, batch)
-			if err != nil {
-				logger.Error("failed-to-notify", err)
-			}
+			logger.Error("failed-to-notify", err)
 		}
 	}
 
