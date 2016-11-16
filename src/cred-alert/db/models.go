d1b9ac5e347bac446b84efcef300e0c7e61e0914
Kris Hicks <khicks@pivotal.io>
2016-11-15 16:55:35 -0800

    Update CredentialCounts tags
    The previous tags worked in development and staging but don't work in
    production. This is a minimal set of tags that actually passes tests
    without PropertyMap blowing up.

--- src/cred-alert/db/models.go
+++ src/cred-alert/db/models.go
@@ -77,7 +77,7 @@ type Repository struct {
 
 	FetchIntervalSeconds int `gorm:"column:fetch_interval"`
 
-	CredentialCounts PropertyMap `sql:"type:longtext;not null;default:'{}'"`
+	CredentialCounts PropertyMap `gorm:"default:'{}'"`
 }
 
 type Fetch struct {
