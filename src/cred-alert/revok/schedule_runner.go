f55145d282d58aeb7081179259b0ae58c9e3551c
Can Berk Güder <cbguder@pivotal.io>
2017-01-10 14:49:51 -0800

    add generic scheduler runner
    Signed-off-by: Christopher Brown <cbrown@pivotal.io>

---
+++ src/cred-alert/revok/schedule_runner.go
@@ -0,0 +1,49 @@
+package revok
+
+import (
+	"os"
+	"sync"
+
+	"github.com/robfig/cron"
+)
+
+type ScheduleRunner struct {
+	cron *cron.Cron
+	cronMut *sync.Mutex
+
+	jobWg *sync.WaitGroup
+}
+
+func NewScheduleRunner() *ScheduleRunner {
+	return &ScheduleRunner{
+		cron: cron.New(),
+		cronMut: &sync.Mutex{},
+		jobWg: &sync.WaitGroup{},
+	}
+}
+
+func (s *ScheduleRunner) Run(signals <-chan os.Signal, ready chan<- struct{}) error {
+	s.cron.Start()
+
+	close(ready)
+
+	select {
+	case <-signals:
+		s.cron.Stop()
+		s.jobWg.Wait()
+	}
+
+	return nil
+}
+
+func (s *ScheduleRunner) ScheduleWork(cron string, work func()) {
+	s.cronMut.Lock()
+	defer s.cronMut.Unlock()
+
+	s.cron.AddFunc(cron, func() {
+		s.jobWg.Add(1)
+		defer s.jobWg.Done()
+
+		work()
+	})
+}
---
+++ src/cred-alert/revok/schedule_runner_test.go
@@ -0,0 +1,72 @@
+package revok_test
+
+import (
+	"github.com/tedsuo/ifrit"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"cred-alert/revok"
+	"os"
+	"sync"
+	"time"
+)
+
+var _ = Describe("Schedule Runner", func() {
+	var (
+		runner *revok.ScheduleRunner
+		process ifrit.Process
+	)
+
+	JustBeforeEach(func() {
+		runner = revok.NewScheduleRunner()
+		process = ifrit.Invoke(runner)
+	})
+
+	AfterEach(func() {
+		process.Signal(os.Kill)
+		<-process.Wait()
+	})
+
+	Describe("scheduling work", func() {
+		It("runs the work on that schedule", func() {
+			wg := &sync.WaitGroup{}
+			wg.Add(2)
+
+			runner.ScheduleWork("@every 1s", func() {
+				wg.Done()
+			})
+
+			done := make(chan struct{})
+
+			go func() {
+				wg.Wait()
+				close(done)
+			}()
+
+			Eventually(done, 3*time.Second).Should(BeClosed())
+		})
+
+		It("does not exit until all the work that is currently in progress has finished", func() {
+			wg := &sync.WaitGroup{}
+			wg.Add(1)
+
+			started := make(chan struct{})
+
+			runner.ScheduleWork("@every 1s", func() {
+				close(started)
+				wg.Wait()
+			})
+
+			<-started
+
+			process.Signal(os.Kill)
+
+			Consistently(process.Wait()).ShouldNot(Receive())
+
+			wg.Done()
+
+			Eventually(process.Wait()).Should(Receive())
+		})
+	})
+})
