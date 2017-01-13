a4de17576fb49be395491cd653ce4ef5d5a0bab3
Christopher Brown <cbrown@pivotal.io>
2017-01-12 16:38:44 -0800

    add rats system test suite
    [finishes #133221693]
    
    Signed-off-by: Can Berk Güder <cbguder@pivotal.io>

---
+++ src/rats/rats_suite_test.go
@@ -0,0 +1,13 @@
+package rats_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestRats(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Rats Suite")
+}
