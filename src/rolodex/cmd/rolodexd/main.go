98fbad2b3d66ed25833f76be757cac4f8f9ca014
Christopher Brown <xoebus@xoeb.us>
2017-03-26 02:12:37 -0700

    use paraphernalia in rolodex

--- src/rolodex/cmd/rolodexd/main.go
+++ src/rolodex/cmd/rolodexd/main.go
@@ -1,30 +1,28 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
 	"log"
-	"net/http"
-	"net/http/pprof"
 	"os"
 
 	"code.cloudfoundry.org/lager"
 	"github.com/jessevdk/go-flags"
+	"github.com/pivotal-cf/paraphernalia/operate/admin"
+	"github.com/pivotal-cf/paraphernalia/secure/tlsconfig"
+	"github.com/pivotal-cf/paraphernalia/serve/grpcrunner"
 	"github.com/tedsuo/ifrit"
 	"github.com/tedsuo/ifrit/grouper"
-	"github.com/tedsuo/ifrit/http_server"
 	"github.com/tedsuo/ifrit/sigmon"
-	"golang.org/x/net/trace"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 
 	"cred-alert/cmdflag"
 	"cred-alert/config"
 	"cred-alert/gitclient"
+	"cred-alert/metrics"
 	"cred-alert/revok"
-	"red/redrunner"
 	"rolodex"
 	"rolodex/rolodexpb"
-	"cred-alert/metrics"
 )
 
 type RolodexOpts struct {
@@ -34,27 +32,9 @@ type RolodexOpts struct {
 func main() {
 	logger := lager.NewLogger("rolodexd")
 	logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.INFO))
+	logger.Info("starting")
 
-	logger.Debug("starting")
-
-	var flagOpts RolodexOpts
-	_, err := flags.Parse(&flagOpts)
-	if err != nil {
-		os.Exit(1)
-	}
-
-	cfg, err := rolodex.LoadRolodexConfig(string(flagOpts.ConfigFile))
-	if err != nil {
-		logger.Fatal("failed-to-load-config", err)
-	}
-
-	errs := cfg.Validate()
-	if errs != nil {
-		for _, err := range errs {
-			fmt.Println(err.Error())
-		}
-		os.Exit(1)
-	}
+	cfg := loadConfig()
 
 	if cfg.Metrics.SentryDSN != "" {
 		logger.RegisterSink(revok.NewSentrySink(cfg.Metrics.SentryDSN, cfg.Metrics.Environment))
@@ -80,62 +60,62 @@ func main() {
 	gitClient := gitclient.New(cfg.GitHub.PrivateKeyPath, cfg.GitHub.PublicKeyPath)
 	syncer := rolodex.NewSyncer(logger, emitter, cfg.RepositoryURL, cfg.RepositoryPath, gitClient, repository)
 
-	grpcServer := redrunner.NewGRPCServer(
+	tlsConfig := tlsconfig.Build(
+		tlsconfig.WithPivotalDefaults(),
+		tlsconfig.WithIdentity(certificate),
+	).Server(tlsconfig.WithClientAuthentication(clientCertPool))
+
+	grpcServer := grpcrunner.NewGRPCServer(
 		logger,
 		fmt.Sprintf("%s:%d", cfg.RPC.BindIP, cfg.RPC.BindPort),
-		&tls.Config{
-			ClientAuth:   tls.RequireAndVerifyClientCert,
-			Certificates: []tls.Certificate{certificate},
-			ClientCAs:    clientCertPool,
-		},
 		func(server *grpc.Server) {
 			rolodexpb.RegisterRolodexServer(server, handler)
 		},
+		grpc.Creds(credentials.NewTLS(tlsConfig)),
 	)
+
+	info := admin.ServiceInfo{
+		Name:        "rolodex",
+		Description: "A service providing information about internal teams.",
+		Team:        "PCF Security Enablement",
+	}
+	debug := admin.Runner("6060", admin.WithInfo(info))
 
 	members := []grouper.Member{
 		{"api", grpcServer},
-		{"debug", http_server.New("127.0.0.1:6060", debugHandler())},
+		{"debug", debug},
 		{"syncer", revok.Schedule(logger, "@every 5m", func() {
 			syncer.Sync()
 		})},
 	}
 
 	runner := sigmon.New(grouper.NewParallel(os.Interrupt, members))
-
 	err = <-ifrit.Invoke(runner).Wait()
 	if err != nil {
 		logger.Fatal("failed-to-start", err)
 	}
 }
 
-func debugHandler() http.Handler {
-	debugRouter := http.NewServeMux()
-	debugRouter.Handle("/debug/pprof/", http.HandlerFunc(pprof.Index))
-	debugRouter.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
-	debugRouter.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
-	debugRouter.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
-	debugRouter.Handle("/debug/pprof/trace", http.HandlerFunc(pprof.Trace))
+func loadConfig() rolodex.RolodexConfig {
+	var flagOpts RolodexOpts
 
-	debugRouter.HandleFunc("/debug/requests", func(w http.ResponseWriter, req *http.Request) {
-		any, sensitive := trace.AuthRequest(req)
-		if !any {
-			http.Error(w, "not allowed", http.StatusUnauthorized)
-			return
+	_, err := flags.Parse(&flagOpts)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	cfg, err := rolodex.LoadRolodexConfig(string(flagOpts.ConfigFile))
+	if err != nil {
+		log.Fatalln("failed to load config", err)
+	}
+
+	errs := cfg.Validate()
+	if errs != nil {
+		for _, err := range errs {
+			fmt.Println(err.Error())
 		}
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		trace.Render(w, req, sensitive)
-	})
+		os.Exit(1)
+	}
 
-	debugRouter.HandleFunc("/debug/events", func(w http.ResponseWriter, req *http.Request) {
-		any, sensitive := trace.AuthRequest(req)
-		if !any {
-			http.Error(w, "not allowed", http.StatusUnauthorized)
-			return
-		}
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		trace.RenderEvents(w, req, sensitive)
-	})
-
-	return debugRouter
+	return *cfg
 }
