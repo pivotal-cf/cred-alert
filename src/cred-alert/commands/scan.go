3d44da2702f1ea9ffa5910dbec49b6c7884ef9f9
Can Berk Güder <cbguder@pivotal.io>
2017-04-13 15:07:34 -0700

    reduce complexity of the scan command
    [#141183803]
    
    Signed-off-by: Christopher Brown <cbrown@pivotal.io>

--- src/cred-alert/commands/scan.go
+++ src/cred-alert/commands/scan.go
@@ -42,137 +42,160 @@ func (command *ScanCommand) Execute(args []string) error {
 		fmt.Fprintln(os.Stderr, yellow("[WARN]"), "Two options specified for Regexp, only using: --regexp", command.Regexp)
 	}
 
-	var sniffer sniff.Sniffer
+	sniffer, err := command.buildSniffer()
+	if err != nil {
+		return err
+	}
+
+	clean := newCleanup()
+	handler := newCredentialCounter(command.ShowCredentials)
+
 	switch {
-	case command.Regexp != "":
-		sniffer = sniff.NewSniffer(matchers.Format(command.Regexp), nil)
-	case command.RegexpFile != "":
-		file, err := os.Open(command.RegexpFile)
+	case command.File != "":
+		if err := command.scanFile(logger, sniffer, handler.HandleViolation, clean); err != nil {
+			return err
+		}
+	case command.Diff:
+		sniffer.Sniff(logger, diffscanner.NewDiffScanner(os.Stdin), handler.HandleViolation)
+	default:
+		sniffer.Sniff(logger, filescanner.New(os.Stdin, "STDIN"), handler.HandleViolation)
+	}
+
+	if handler.count > 0 {
+		showCredentialWarning()
+		clean.exit(3)
+	}
+
+	return nil
+}
+
+func (c *ScanCommand) scanFile(logger lager.Logger, sniffer sniff.Sniffer, handleFunc sniff.ViolationHandlerFunc, cleaner *cleanup) error {
+	fi, err := os.Stat(c.File)
+	if err != nil {
+		return err
+	}
+
+	violationsDir, err := ioutil.TempDir("", "cred-alert-cli-violations")
+	if err != nil {
+		return err
+	}
+
+	inflateDir, err := ioutil.TempDir("", "cred-alert-cli")
+	if err != nil {
+		return err
+	}
+
+	quietLogger := kolsch.NewLogger()
+	archiveHandler := sniff.NewArchiveViolationHandlerFunc(inflateDir, violationsDir, handleFunc)
+	scanner := dirscanner.New(sniffer, handleFunc, archiveHandler, inflateDir)
+
+	if fi.IsDir() {
+		return scanner.Scan(quietLogger, c.File)
+	}
+
+	file, err := os.Open(c.File)
+	if err != nil {
+		return err
+	}
+
+	br := bufio.NewReader(file)
+	if mime, isArchive := mimetype.IsArchive(logger, br); isArchive {
+		inflate := inflator.New()
+		cleaner.register(func() {
+			inflate.Close()
+			os.RemoveAll(inflateDir)
+		})
+
+		inflateArchive(quietLogger, inflate, inflateDir, mime, c.File)
+
+		err = scanner.Scan(quietLogger, inflateDir)
 		if err != nil {
 			return err
 		}
-		defer file.Close()
+	} else {
+		sniffer.Sniff(logger, filescanner.New(br, c.File), handleFunc)
+	}
+
+	return nil
+}
+
+func (c *ScanCommand) buildSniffer() (sniff.Sniffer, error) {
+	var sniffer sniff.Sniffer
+
+	switch {
+	case c.Regexp != "":
+		sniffer = sniff.NewSniffer(matchers.Format(c.Regexp), nil)
+	case c.RegexpFile != "":
+		file, err := os.Open(c.RegexpFile)
+		if err != nil {
+			return nil, err
+		}
 
 		matcher := matchers.UpcasedMultiMatcherFromReader(file)
+
+		if err := file.Close(); err != nil {
+			return nil, err
+		}
+
 		sniffer = sniff.NewSniffer(matcher, nil)
 	default:
 		sniffer = sniff.NewDefaultSniffer()
 	}
 
+	return sniffer, nil
+}
+
+type cleanup struct {
+	work []func()
+}
+
+func newCleanup() *cleanup {
+	clean := &cleanup{}
+
 	signalsCh := make(chan os.Signal)
 	signal.Notify(signalsCh, os.Interrupt)
 
-	var exitFuncs []func()
 	go func() {
 		<-signalsCh
 		log.SetFlags(0)
 		log.Println("\ncleaning up...")
-		for _, f := range exitFuncs {
-			f()
-		}
-		os.Exit(1)
+		clean.exit(1)
 	}()
 
-	var credsFound int
-	handler := func(logger lager.Logger, violation scanners.Violation) error {
-		line := violation.Line
-		credsFound++
-		output := fmt.Sprintf("%s %s:%d", red("[CRED]"), line.Path, line.LineNumber)
-		if command.ShowCredentials {
-			output = output + fmt.Sprintf(" [%s]", violation.Credential())
-		}
-		fmt.Println(output)
+	return clean
+}
 
-		return nil
+func (c *cleanup) register(fn func()) {
+	c.work = append(c.work, fn)
+}
+
+func (c cleanup) exit(status int) {
+	for _, w := range c.work {
+		w()
 	}
 
-	quietLogger := kolsch.NewLogger()
+	os.Exit(status)
+}
 
-	switch {
-	case command.File != "":
-		fi, err := os.Stat(command.File)
-		if err != nil {
-			return err
-		}
-
-		violationsDir, err := ioutil.TempDir("", "cred-alert-cli-violations")
-		if err != nil {
-			return err
-		}
-
-		inflateDir, err := ioutil.TempDir("", "cred-alert-cli")
-		if err != nil {
-			return err
-		}
-
-		archiveHandler := sniff.NewArchiveViolationHandlerFunc(inflateDir, violationsDir, handler)
-		scanner := dirscanner.New(sniffer, handler, archiveHandler, inflateDir)
-		if fi.IsDir() {
-			err = scanner.Scan(quietLogger, command.File)
-			if err != nil {
-				return err
-			}
-		} else {
-			file, err := os.Open(command.File)
-			if err != nil {
-				return err
-			}
-
-			br := bufio.NewReader(file)
-			if mime, isArchive := mimetype.IsArchive(logger, br); isArchive {
-
-				inflate := inflator.New()
-				exitFuncs = append(exitFuncs, func() {
-					inflate.Close()
-					os.RemoveAll(inflateDir)
-				})
-
-				inflateArchive(quietLogger, inflate, inflateDir, mime, command.File)
-
-				err = scanner.Scan(quietLogger, inflateDir)
-				if err != nil {
-					return err
-				}
-			} else {
-				sniffer.Sniff(logger, filescanner.New(br, command.File), handler)
-			}
-		}
-	case command.Diff:
-		sniffer.Sniff(logger, diffscanner.NewDiffScanner(os.Stdin), handler)
-	default:
-		sniffer.Sniff(logger, filescanner.New(os.Stdin, "STDIN"), handler)
-	}
-
-	if credsFound > 0 {
-		fmt.Println()
-		fmt.Println("Yikes! Looks like we found some credentials.")
-		fmt.Println()
-		fmt.Println("There are a few cases for what this may be:")
-		fmt.Println()
-		fmt.Println("1. An actual credential in a repository which shouldn't be")
-		fmt.Println("   committed! Remove it and try committing again.")
-		fmt.Println()
-		fmt.Println("2. An example credential in tests or documentation. You can")
-		fmt.Println("   use the words 'fake' and/or 'example' in your credential so it is")
-		fmt.Println("   ignored.")
-		fmt.Println()
-		fmt.Println("3. An actual credential in a credential repository. If you are calling this")
-		fmt.Println("   via Git hook and if you want the false positive to go away, you can pass `-n`")
-		fmt.Println("   to skip the hook for now.")
-		fmt.Println()
-		fmt.Println("4. A false positive which isn't a credential at all! Please let us know about ")
-		fmt.Println("   the this case in our Slack channel (#pcf-sec-enablement).")
-
-		exitFuncs = append(exitFuncs, func() {
-			os.Exit(3)
-		})
-	}
-
-	for _, f := range exitFuncs {
-		f()
-	}
-
-	return nil
+func showCredentialWarning() {
+	fmt.Println()
+	fmt.Println("Yikes! Looks like we found some credentials.")
+	fmt.Println()
+	fmt.Println("There are a few cases for what this may be:")
+	fmt.Println()
+	fmt.Println("1. An actual credential in a repository which shouldn't be")
+	fmt.Println("   committed! Remove it and try committing again.")
+	fmt.Println()
+	fmt.Println("2. An example credential in tests or documentation. You can")
+	fmt.Println("   use the words 'fake' and/or 'example' in your credential so it is")
+	fmt.Println("   ignored.")
+	fmt.Println()
+	fmt.Println("3. An actual credential in a credential repository. If you are calling this")
+	fmt.Println("   via Git hook and if you want the false positive to go away, you can pass `-n`")
+	fmt.Println("   to skip the hook for now.")
+	fmt.Println()
+	fmt.Println("4. A false positive which isn't a credential at all! Please let us know about ")
+	fmt.Println("   the this case in our Slack channel (#pcf-sec-enablement).")
 }
 
 func inflateArchive(
@@ -217,4 +240,27 @@ func warnIfOldExecutable() {
 	if time.Since(mtime) > twoWeeks {
 		fmt.Fprintln(os.Stderr, yellow("[WARN]"), "Executable is old! Please consider running `cred-alert-cli update`.")
 	}
+}
+
+func newCredentialCounter(showCreds bool) *credentialCounter {
+	return &credentialCounter{
+		showCreds: showCreds,
+	}
+}
+
+type credentialCounter struct {
+	count     int
+	showCreds bool
+}
+
+func (c *credentialCounter) HandleViolation(logger lager.Logger, violation scanners.Violation) error {
+	line := violation.Line
+	c.count++
+	output := fmt.Sprintf("%s %s:%d", red("[CRED]"), line.Path, line.LineNumber)
+	if c.showCreds {
+		output = output + fmt.Sprintf(" [%s]", violation.Credential())
+	}
+	fmt.Println(output)
+
+	return nil
 }
