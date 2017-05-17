d35b4c03deec15fb23db78bd5612f91af7a6fe57
Christopher Brown <cbrown@pivotal.io>
2017-05-17 10:02:40 -0700

    seems like spans with the same name overwrite each other

--- src/cred-alert/notifications/slack_notifier.go
+++ src/cred-alert/notifications/slack_notifier.go
@@ -112,8 +112,7 @@ func (n *slackNotifier) makeSingleAttempt(ctx context.Context, logger lager.Logg
 
 	// We manually trace this to avoid the URL leaking in the built in HTTP
 	// tracing.
-	span := trace.FromContext(ctx).NewChild("slack-request")
-	span.SetLabel("attempt", strconv.Itoa(currentAttempt+1))
+	span := trace.FromContext(ctx).NewChild(fmt.Sprintf("slack-request-attempt-%d", currentAttempt+1))
 
 	resp, err := n.client.Do(req)
 	if err != nil {
